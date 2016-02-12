e892ad3397e8efcd211e55115f047615bad0df3a
Michael Tran <minhtran1995@hotmail.com>
2016-02-12 01:20:05 -0500

    Fix contact Page - Adding Hyperlink

--- public/js/typingAnimation.js
+++ public/js/typingAnimation.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".element").typed({
-        strings: ["Hello. !", "Xin Chào. !", "Bonjour . !", "Hello. !", "你好 . !", "Namaste . !", "Hello. !", "안녕 . !", "Привет . !","Hello. !"],
+        strings: ["Hello !", "Xin Chào !", "Bonjour  !", "Hello !", "你好  !", "Namaste  !", "Hello !", "안녕  !", "Привет  !","Hello !"],
         typeSpeed: 0.1,
         startDelay: 2000,
         backDelay: 3000,
